2e2629853fbaf1a471c3c86b27168652eec7122a
solanav <solanav@qq.com>
2019-12-14 18:55:40 +0100

    Added docs

---
+++ docs/tests.md
@@ -0,0 +1,34 @@
+# Arquitectura general del codigo
+
+
+# Resultados de los tests
+
+## Test 0
+Original
+![](test_0_og.jpg)
+
+Expected
+![](test_0_solved.jpg)
+
+Results
+![](0.png)
+
+## Test 1
+Original
+![](test_1_og.png)
+
+Expected
+![](test_1_solved.png)
+
+Results
+![](1.png)
+
+## Test 2
+Original
+![](test_2_og.png)
+
+Expected
+![](test_2_solved.png)
+
+Results
+![](2.png)
--- src/main.c
+++ src/main.c
@@ -7,7 +7,81 @@
 #include "../include/types.h"
 #include "../include/minimiza.h"
 
-AFND *AFND_TEST()
+AFND *AFND_TEST0()
+{
+
+	AFND *p_afnd;
+	p_afnd = AFNDNuevo("afnd_test", 6, 2);
+
+	AFNDInsertaSimbolo(p_afnd, "0");
+	AFNDInsertaSimbolo(p_afnd, "1");
+
+	AFNDInsertaEstado(p_afnd, "A", INICIAL);
+	AFNDInsertaEstado(p_afnd, "B", NORMAL);
+	AFNDInsertaEstado(p_afnd, "C", FINAL);
+	AFNDInsertaEstado(p_afnd, "D", FINAL);
+	AFNDInsertaEstado(p_afnd, "E", FINAL);
+	AFNDInsertaEstado(p_afnd, "F", NORMAL);
+	
+	AFNDInsertaTransicion(p_afnd, "A", "0", "B");
+	AFNDInsertaTransicion(p_afnd, "A", "1", "C");
+
+	AFNDInsertaTransicion(p_afnd, "B", "0", "A");
+	AFNDInsertaTransicion(p_afnd, "B", "1", "D");
+
+	AFNDInsertaTransicion(p_afnd, "C", "0", "E");
+	AFNDInsertaTransicion(p_afnd, "C", "1", "F");
+
+	AFNDInsertaTransicion(p_afnd, "D", "0", "E");
+	AFNDInsertaTransicion(p_afnd, "D", "1", "F");
+
+	AFNDInsertaTransicion(p_afnd, "E", "0", "E");
+	AFNDInsertaTransicion(p_afnd, "E", "1", "F");
+
+	AFNDInsertaTransicion(p_afnd, "F", "0", "F");
+	AFNDInsertaTransicion(p_afnd, "F", "1", "F");
+
+	AFNDCierraLTransicion(p_afnd);
+
+	return p_afnd;
+}
+
+AFND *AFND_TEST1()
+{
+
+	AFND *p_afnd;
+	p_afnd = AFNDNuevo("afnd_test", 5, 2);
+
+	AFNDInsertaSimbolo(p_afnd, "0");
+	AFNDInsertaSimbolo(p_afnd, "1");
+
+	AFNDInsertaEstado(p_afnd, "A", INICIAL);
+	AFNDInsertaEstado(p_afnd, "B", NORMAL);
+	AFNDInsertaEstado(p_afnd, "C", NORMAL);
+	AFNDInsertaEstado(p_afnd, "D", NORMAL);
+	AFNDInsertaEstado(p_afnd, "E", FINAL);
+	
+	AFNDInsertaTransicion(p_afnd, "A", "0", "B");
+	AFNDInsertaTransicion(p_afnd, "A", "1", "C");
+	
+	AFNDInsertaTransicion(p_afnd, "B", "0", "B");
+	AFNDInsertaTransicion(p_afnd, "B", "1", "D");
+	
+	AFNDInsertaTransicion(p_afnd, "C", "0", "B");
+	AFNDInsertaTransicion(p_afnd, "C", "1", "C");
+	
+	AFNDInsertaTransicion(p_afnd, "D", "0", "B");
+	AFNDInsertaTransicion(p_afnd, "D", "1", "E");
+	
+	AFNDInsertaTransicion(p_afnd, "E", "0", "B");
+	AFNDInsertaTransicion(p_afnd, "E", "1", "C");
+	
+	AFNDCierraLTransicion(p_afnd);
+
+	return p_afnd;
+}
+
+AFND *AFND_TEST2()
 {
 
 	AFND *p_afnd;
@@ -59,7 +133,7 @@ int main(int argc, char **argv)
 {
 	AFND *afd;
 	AFND *afd_min;
-	AFND *p_afnd = AFND_TEST();
+	AFND *p_afnd = AFND_TEST2();
 
 	// Removing inaccessible nodes
 	afd = AFNDTransforma(p_afnd);
